4ebad01ce5104e834ffddfeb50b84be5670ce3ba
vermalisa11 <vermalisa11@gmail.com>
2021-05-14 14:48:12 +0530

    add frontend

--- public/bundle.js
+++ public/bundle.js
@@ -50,5 +50,32 @@ var web3 = new Web3('http://localhost:9545');
 var simpleStorage = new web3.eth.Contract(simpleStorageABI, simpleStorageAddress);
 
 document.addEventListener('DOMContentLoaded', () => {
-    
+  const $setData = document.getElementById('setData');
+  const $data = document.getElementById('data');
+  let accounts =[];
+
+  web3.eth.getAccounts()
+    .then(_accounts => {
+      accounts = _accounts;
+  });
+  
+  const getData = () => {
+    simpleStorage.methods
+      .get()
+      .call()
+      .then(result => {
+        $data.innerHTML = result;
+    });
+  };
+  getData();
+
+  $setData.addEventListener('submit', e =>{
+    e.preventDefault();
+    const data = e.target.elements[0].value;
+    simpleStorage.methods
+      .set(data)
+      .send({from: accounts[0]})
+      .then(getData);
+  });
+  
 });
